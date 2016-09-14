ef3bafb406407d6cc2dee3da83af39df40f6c2cf
Aditya Bhardwaj <abhardw2@ncsu.edu>
2016-09-13 23:56:02 -0400

    Added program to calculate power of 2 nearest to a particular value input

---
+++ powerofTwo.py
@@ -0,0 +1,20 @@
+
+# function to calculate the power of 2 nearest to the value of the input
+
+def eval_power(val):
+    val1 = 1
+
+    while val1 <= val:
+        val1 *= 2
+
+    v = val1/2
+    z = 1
+    while v/2 > 1:
+        v = v/2
+        z += 1
+
+    return (val1/2),z
+
+
+
+
